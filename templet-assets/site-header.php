640cbac3afc93e841b63e9de248c6dad1821acd3
Prathamesh Bhagat <prathameshbhagat1511@gmail.com>
2025-11-16 13:15:13 +0530

    Update site-header.php

--- templet-assets/site-header.php
+++ templet-assets/site-header.php
@@ -1,1 +1,14 @@
+<header class="site-header">
+  <div class="container">
+    <?php
+      if ( function_exists('the_custom_logo') ) {
+          the_custom_logo();
+      }
+    ?>
+
+    <nav class="main-navigation">
+      <?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
+    </nav>
+  </div>
+</header>
 
