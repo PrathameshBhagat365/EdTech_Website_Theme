9a3cea6cd23ac678dfc3bf088f66b946a3c0ce6f
Prathamesh Bhagat <prathameshbhagat1511@gmail.com>
2025-11-17 13:37:36 +0530

    Add course duration and level to course meta

--- templet-assets/course/course-meta.php
+++ templet-assets/course/course-meta.php
@@ -1,1 +1,5 @@
+<div class="course-meta">
+  <span>Duration: <?php echo get_post_meta(get_the_ID(), 'course_duration', true); ?></span>
+  <span>Level: <?php echo get_post_meta(get_the_ID(), 'course_level', true); ?></span>
+</div>
 
