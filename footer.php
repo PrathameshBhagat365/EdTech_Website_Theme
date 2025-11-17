2d1a009b87772a2932a6a714b04a05066262c1a1
Prathamesh Bhagat <prathameshbhagat1511@gmail.com>
2025-11-17 13:44:36 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+</main> <!-- End of site-content -->
+
+<footer class="site-footer-wrapper">
+    <?php get_template_part('template-parts/footer/site-footer'); ?>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
