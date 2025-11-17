144d3acd7b0039ed2e5972b65fcbd55174796e54
Prathamesh Bhagat <prathameshbhagat1511@gmail.com>
2025-11-17 13:44:03 +0530

    Add initial header.php file structure

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php wp_body_open(); ?>
+
+<header class="site-header-wrapper">
+    <?php get_template_part('template-parts/header/site-header'); ?>
+</header>
+
+<main class="site-content">
