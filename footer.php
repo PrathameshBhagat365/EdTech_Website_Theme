7a0dd00c2bbbc1914832c47ce342e0c7dfa5bb5a
Prathamesh Bhagat <prathameshbhagat1511@gmail.com>
2025-12-05 15:34:31 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-</main> <!-- End of site-content -->
-
-<footer class="site-footer-wrapper">
-    <?php get_template_part('template-parts/footer/site-footer'); ?>
-</footer>
-
-<?php wp_footer(); ?>
-</body>
-</html>
